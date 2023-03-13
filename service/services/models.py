0b2ddf005fb90d38d7a3e3c99bbb68c8db66d9ca
maxRakh <maxmaxutmax@gmail.com>
2023-03-13 10:21:54 +0300

    Part 5

--- service/services/models.py
+++ service/services/models.py
@@ -2,6 +2,7 @@ from django.core.validators import MaxValueValidator
 from django.db import models
 
 from clients.models import Client
+from services.tasks import set_price
 
 
 class Service(models.Model):
@@ -10,6 +11,15 @@ class Service(models.Model):
 
     def __str__(self):
         return f"Service: {self.name}"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__full_price = self.full_price
+
+    def save(self, *args, **kwargs):
+        if self.__full_price != self.full_price:
+            for subscription in self.subscriptions.all():
+                set_price.delay(subscription.id)
 
 
 class Plan(models.Model):
@@ -25,14 +35,27 @@ class Plan(models.Model):
                                                             MaxValueValidator(100)
                                                         ])
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__discount_percent = self.discount_percent
+
     def __str__(self):
         return f"Plan: {self.plan_type}, {self.discount_percent}%"
 
+    def save(self, *args, **kwargs):
+        if self.__discount_percent != self.discount_percent:
+            for subscription in self.subscriptions.all():
+                set_price.delay(subscription.id)
+
+        return super().save(*args, **kwargs)
+
 
 class Subscription(models.Model):
-    client = models.ForeignKey(Client, related_name='subscription', on_delete=models.PROTECT)
-    service = models.ForeignKey(Service, related_name='subscription', on_delete=models.PROTECT)
-    plan = models.ForeignKey(Plan, related_name='subscription', on_delete=models.PROTECT)
+    client = models.ForeignKey(Client, related_name='subscriptions', on_delete=models.PROTECT)
+    service = models.ForeignKey(Service, related_name='subscriptions', on_delete=models.PROTECT)
+    plan = models.ForeignKey(Plan, related_name='subscriptions', on_delete=models.PROTECT)
+    price = models.PositiveSmallIntegerField(default=0)
 
     def __str__(self):
         return f"{self.client} - {self.service}"
+
